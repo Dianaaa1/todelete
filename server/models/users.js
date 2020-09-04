1c127f4ff88c280bfecd37541c0fa340a8dbc6db
Dianaaa1 <dianasamkova20@gmail.com>
2020-09-04 12:50:05 +0300

    ошибка при авторизации

--- server/models/users.js
+++ server/models/users.js
@@ -1,37 +1,35 @@
 const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
 
+mongoose.set("useCreateIndex", true);
 
-mongoose.set('useCreateIndex', true)
+mongoose
+  .connect("mongodb://localhost/MyDatabase", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("DB Connected!"))
+  .catch((err) => {
+    console.log(`DB Connection Error: ${err.message}`);
+  });
+
 const userSchema = new mongoose.Schema({
   username: {
-      type: String,
+    type: String,
+    unique: true,
+    require: true,
+    index: {
       unique: true,
-      require: true,
-      index: {
-          unique: true,
-      }
+    },
   },
   password: String,
 });
-mongoose.connect('mongodb://127.0.0.1:27017/users', {
-useUnifiedTopology: true,
-useNewUrlParser: true,
-})
-.then(() => console.log('DB Connected!'))
-.catch(err => {
-console.log(`DB Connection Error: ${err.message}`);
-});
-
-
 userSchema.plugin(passportLocalMongoose);
 
 // User Schema model
-const User = new mongoose.model("User", userSchema);
+const User = new mongoose.model('User', userSchema, 'User');
 
-const user = new User({ username: "nick", password: "nick18", index: 0 });
-user.save(function(err, doc) {
-    if (err) return console.error(err);
-    console.log("Document inserted succussfully!");
-  })
+//const user = new User({ username: "nick", password: "nick18"});
+User.register({username:'zzzz', active: false},'zzzz111111111111111');
+
 module.exports = User;
